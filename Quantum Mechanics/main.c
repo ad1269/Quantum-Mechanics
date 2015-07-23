bb213e8f7585a74b8ebca9a2b71e515b4f493bd3
ad1269 <admohanraj@gmail.com>
2015-07-22 21:34:10 -0700

    Initial Commit

---
+++ Quantum Mechanics/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quantum Mechanics
+//
+//  Created by Mohan Lakshmanan on 7/22/15.
+//  Copyright (c) 2015 AD. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
